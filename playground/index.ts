286c4e248eb865ebf1f1e0f1e975c584d6d7f49f
zaneliu <ezreeeeal@gmail.com>
2024-03-30 01:18:57 +0800

    chore(playground): update Playground

--- playground/index.ts
+++ playground/index.ts
@@ -1,3 +1,9 @@
-import { test } from "../src";
+import { Polygon } from "pixi.js";
+import { polygonsOverlap } from "../src";
 
-console.log(test());
+console.log(
+  polygonsOverlap(
+    new Polygon(0, 0, 0, 1, 1, 1, 1, 0),
+    new Polygon(0.5, 0.5, 0.5, 1.5, 1.5, 1.5, 1.5, 0.5),
+  ),
+);
